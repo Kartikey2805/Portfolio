b45e2f38acd763d112b62ede9862de16c0ac6263
Kartikey Goel <kartikeygoel093@gmail.com>
2023-03-27 23:55:38 +0530

    added description, images and source code urls

--- main.py
+++ main.py
@@ -16,14 +16,33 @@ with col2:
     """
     st.info(content)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5,0.5,1.5])
 
 df = pandas.read_csv('data.csv',sep=';')
 
 with col3:
     for index,row in df[:10].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image(f"images/{row['image']}")
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
-    for index,row in df[:10].iterrows():
+    for index,row in df[10:].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image(f"images/{row['image']}")
+        st.write(f"[Source Code]({row['url']})")
+
+st.markdown(
+    """
+    <style>
+
+    img {
+      border-radius:50%;
+    }
+
+    </style>
+
+    """
+)
