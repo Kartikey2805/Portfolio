ee3eaf0cb0e9afd994e2efa74e3d624b03bf82da
Kartikey Goel <kartikeygoel093@gmail.com>
2023-03-28 01:01:40 +0530

    created send email script

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,10 +1,17 @@
 import streamlit as st
+from send_email import send_email
 
 st.header('Contact Me')
 
 with st.form(key='email_form'):
     user_email = st.text_input('Your email address')
-    message = st.text_area('Your message')
+    message_text = st.text_area('Your message')
+    message = f"""\
+Subject: Feedback from {user_email}
+
+From: {user_email}
+{message_text}"""
     button = st.form_submit_button()
     if button: # button is a boolean
-        print('submit')
+        send_email(message)
+        st.info('Your email was sent successfully')
--- send_email.py
+++ send_email.py
@@ -1,20 +1,21 @@
 import smtplib,ssl
 
-host = 'smtp.gmail.com'
-port = 465
+def send_email(message):
+    host = 'smtp.gmail.com'
+    port = 465
 
-username =  'kartikey.goel.285@gmail.com'
-password =  'rmhfrwhhusshohjt'
-receiver = 'kartikey.goel.285@gmail.com'
+    username =  'kartikey.goel.285@gmail.com'
+    password =  'rmhfrwhhusshohjt'
+    receiver = 'kartikey.goel.285@gmail.com'
 
-message = """\
-Subject:Hi!
-How are you?
-Bye!
-"""
+    # message = """\
+    # Subject:Hi!
+    # How are you?
+    # Bye!
+    # """
 
-context = ssl.create_default_context()
+    context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL(host,port,context=context) as server:
-    server.login(username,password)
-    server.sendmail(username,receiver,message)
+    with smtplib.SMTP_SSL(host,port,context=context) as server:
+        server.login(username,password)
+        server.sendmail(username,receiver,message)
